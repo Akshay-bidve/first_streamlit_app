19e38b84119285a0b792554672038d74924741cd
Akshay-bidve <115452676+Akshay-bidve@users.noreply.github.com>
2022-10-10 15:15:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,8 @@ streamlit.text('🐔 Hard boiled free-Range Egg')
 streamlit.text('🥑🍞 Avacaddo toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas as pd
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamline.dataframe(my_fruit_list)
