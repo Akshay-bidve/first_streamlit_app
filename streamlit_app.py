a8e132603fc73f285fd4c241ebe1c9d64a34b23e
Akshay-bidve <115452676+Akshay-bidve@users.noreply.github.com>
2022-10-10 15:09:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
-streamlit.title('My parents new healthy dinner 🥣 🥗 🐔 🥑🍞')
-streamlit.header('Breakfast menu')
+streamlit.title('My parents new healthy dinner ')
+streamlit.header('Breakfast menu 🥣 🥗 🐔 🥑🍞')
 streamlit.text('Omega 3 & blueberry oatmeal')
 streamlit.text(' Kale, spinach & rocket Smoothie')
 streamlit.text('Hard boiled free-Range Egg')
